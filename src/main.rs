1ccaa253538c66d317c562cb662d10ba724908b6
SasakiSaki <galaster@foxmail.com>
2020-08-01 12:16:58 +0800

    🌿 Update main.rs (#1)

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,6 @@
 
 use std::{fs, fs::File, io::Write};
 
-#[allow(unused_imports)]
-use clap::{App, Arg};
 use glob::glob;
 use serde::Deserialize;
 use toml;
@@ -30,38 +28,18 @@ impl Default for Config {
 }
 
 fn main() -> Result<(), Error> {
-    App::new("PNG Cleaner")
-        .version(env!("CARGO_PKG_VERSION"))
-        .author(env!("CARGO_PKG_AUTHORS"))
-        .about(env!("CARGO_PKG_DESCRIPTION"))
-        .get_matches();
-    let cfg: Config = match toml::from_slice(&fs::read("pngc.toml")?) {
-        Ok(o) => o,
-        Err(_) => Config::default(),
-    };
+    let mut cfg = Config::default();
+    if let Ok(file) = &fs::read("pngc.toml") {
+        if let Ok(o) = toml::from_slice(file) {
+            cfg = o
+        };
+    }
     let mut file = File::create("pngc.csv")?;
-    file.write_all("路径,大小(MB),异常等级\n".as_bytes())?;
+    file.write_all("路径,大小(KB),异常等级\n".as_bytes())?;
     for entry in glob(&cfg.glob)? {
         if let Ok(w) = check_file(entry, &cfg) {
             file.write_all(w.as_bytes())?;
         }
     }
     Ok(())
-    /*
-    let input = matches.value_of("INPUT").unwrap();
-
-    let value = parse_file(input, matches.value_of("File Format"))?;
-    match matches.occurrences_of("Text") {
-        0 => (),
-        _ => write_to_file(&format!("{}.m", input), value.to_string().as_bytes())?,
-    }
-    match matches.occurrences_of("Binary") {
-        0 => (),
-        _ => write_to_file(&format!("{}.wxf", input), &value.to_bytes())?,
-    }
-    match matches.occurrences_of("Compress") {
-        0 => (),
-        _ => write_to_file(&format!("{}.mx", input), &value.to_compressed())?,
-    };
-    */
 }
